f171e7291f4a0c9668c949e1adfbab5ccfa461c3
Lauri Rooden <lauri@rooden.ee>
2013-05-28 01:20:31 +0300

    Share only custom cache by default

--- functional-lite.js
+++ functional-lite.js
@@ -2,7 +2,7 @@
 
 
 /*
-* @version  0.0.9
+* @version  0.0.10
 * @author   Lauri Rooden - https://github.com/litejs/fn-lite
 * @license  MIT License  - http://lauri.rooden.ee/mit-license.txt
 */
@@ -80,7 +80,7 @@ function Init()  {
 		f.origin = self
 		f.cached = c
 		f.extend = function() {
-			return self.extend.apply(self, arguments).cache(instance, keyFn)
+			return self.extend.apply(self, arguments).cache(instance, keyFn, cache)
 		}
 		f[P] = self[P] // prototype for better access on extending 
 		return f

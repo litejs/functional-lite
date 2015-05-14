240b989ce9fa84b1317d25a10ac652e2a7f5a597
Lauri Rooden <lauri@rooden.ee>
2015-05-15 00:30:15 +0300

    v0.3.3

--- README.md
+++ README.md
@@ -10,8 +10,8 @@
 
 
 
-    @version    0.3.2
-    @date       2015-05-11
+    @version    0.3.3
+    @date       2015-05-14
     @stability  2 - Unstable
 
 
--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 
 /*
- * @version    0.3.2
- * @date       2015-05-11
+ * @version    0.3.3
+ * @date       2015-05-14
  * @stability  2 - Unstable
  * @author     Lauri Rooden <lauri@rooden.ee>
  * @license    MIT License

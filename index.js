ac1ab03945fe98877b2c3fd3e8aff5a5d89e369d
Lauri Rooden <lauri@rooden.ee>
2016-01-15 07:45:19 +0200

    Add typeof to fnRe

--- index.js
+++ index.js
@@ -17,7 +17,7 @@
 	, hasOwn = Object.prototype.hasOwnProperty
 	, slice = F.call.bind(A.slice)
 	, fns = {}
-	, fnRe = /(['\/"])(?:\\?.)*?\1|\b(?:false|in|new|null|this|true|void)\b|\.\w+|\w+:/g
+	, fnRe = /(['\/"])(?:\\?.)*?\1|\b(?:false|in|new|null|this|true|typeof|void)\b|\.\w+|\w+:/g
 
 
 
